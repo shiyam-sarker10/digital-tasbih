dfadc9e73ecf7bae9097112380afcd34a1a41126
shiyam-sarker10 <kaneki438500929@gmail.com>
2023-08-17 20:52:56 +0600

    version 4.0

--- js/modifty.js
+++ js/modifty.js
@@ -5,32 +5,20 @@ function increment(target) {
     return alert(`The  tasbih count for ${name} is finish`), (initial = 0);
   }
   initial += 1;
-  const count = (target.parentNode.parentNode.childNodes[3].innerText =
-    initial);
+  const count = (target.parentNode.parentNode.childNodes[3].innerText =initial);
 }
-
 function decrement(target) {
   const name = target.parentNode.parentNode.childNodes[1].innerText;
   if (initial <= 0) {
     return alert("Please click on increment and start you todays tasbih");
   }
   initial--;
-  const count = (target.parentNode.parentNode.childNodes[3].innerText =
-    initial);
+  const count = (target.parentNode.parentNode.childNodes[3].innerText =initial);
 }
-
 function reset(target) {
-  function childNumber(number) {
-    const resetItem = (target.parentNode.parentNode.childNodes[5].childNodes[
-      number
-    ].childNodes[3].innerText = initial);
-    return resetItem;
-  }
   initial = 0;
-  const resetItem = childNumber(1);
-
-  const resetItem1 = childNumber(5);
-
-  const resetItem2 = childNumber(9);
+  const resetItem =(target.parentNode.parentNode.childNodes[5].childNodes[1].childNodes[3].innerText=initial);
+  const resetItem1 =(target.parentNode.parentNode.childNodes[5].childNodes[5].childNodes[3].innerText=initial)
+  const resetItem2 =(target.parentNode.parentNode.childNodes[5].childNodes[9].childNodes[3].innerText=initial)
 }
 // modified version
